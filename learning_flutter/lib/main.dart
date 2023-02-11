38646d8503da63bbb7c2bb2e1192eddc5e8cb7d9
Abdulrahman Mohammed <abdulrahman48811@gmail.com>
2023-02-11 15:35:34 -0800

    Random Words

--- learning_flutter/lib/main.dart
+++ learning_flutter/lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
 
@@ -9,8 +11,18 @@ class MyApp extends StatelessWidget {
     final wordPair = WordPair.random();
     return MaterialApp(
         theme: ThemeData(primaryColor: Colors.purple[900]),
-        home: Scaffold(
-            appBar: AppBar(title: Text('WordPair Generator')),
-            body: Center(child: Text(wordPair.asPascalCase))));
+        home: RandomWords());
+  }
+}
+
+class RandomWords extends StatefulWidget {
+  @override
+  RandomWordsState createState() => RandomWordsState();
+}
+
+class RandomWordsState extends State<RandomWords> {
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: Text('WordPair Generator')), body: Text('Hello'));
   }
 }
